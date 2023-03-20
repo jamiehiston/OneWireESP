abb244fa03c03653c05542f234dfc5cb1c450a96
jamiehiston <38505716+jamiehiston@users.noreply.github.com>
2023-03-20 09:35:22 +0000

    add support for 2 buses

--- OneWireESP.h
+++ OneWireESP.h
@@ -56,7 +56,8 @@
 
 
 //== Define your OneWire GPIO pin here ==//
-#define OW1_PIN GPIO_NUM_9
+#define OW1_PIN GPIO_NUM_11
+#define OW2_PIN GPIO_NUM_9
 
 
 class OneWire
@@ -77,52 +78,66 @@ class OneWire
     OneWire() { }
     OneWire(gpio_num_t pin) { begin(pin); }
     void begin(gpio_num_t pin);
-
+    
     // Perform a 1-Wire reset cycle. Returns 1 if a device responds
     // with a presence pulse.  Returns 0 if there is no device or the
     // bus is shorted or otherwise held low for more than 250uS
-    uint8_t reset(void);
+    //uint8_t reset(void);
+    uint8_t reset1(void);
+    uint8_t reset2(void);
 
     // Issue a 1-Wire rom select command, you do the reset first.
-    void select(const uint8_t rom[8]);
+    void select1(const uint8_t rom[8]);
+    void select2(const uint8_t rom[8]);
 
     // Issue a 1-Wire rom skip command, to address all on bus.
-    void skip(void);
+    void skip1(void);
+    void skip2(void);
 
     // Write a byte. If 'power' is one then the wire is held high at
     // the end for parasitically powered devices. You are responsible
     // for eventually depowering it by calling depower() or doing
     // another read or write.
-    void write(uint8_t v, uint8_t power = 0);
+    void write1(uint8_t v, uint8_t power = 0);
+    void write2(uint8_t v, uint8_t power = 0);
 
-    void write_bytes(const uint8_t *buf, uint16_t count, bool power = 0);
+    void write_bytes1(const uint8_t *buf, uint16_t count, bool power = 0);
+    void write_bytes2(const uint8_t *buf, uint16_t count, bool power = 0);
 
     // Read a byte.
-    uint8_t read(void);
+    uint8_t read1(void);
+    uint8_t read2(void);
 
-    void read_bytes(uint8_t *buf, uint16_t count);
+    void read_bytes1(uint8_t *buf, uint16_t count);
+    void read_bytes2(uint8_t *buf, uint16_t count);
 
     // Write a bit. The bus is always left powered at the end, see
     // note in write() about that.
-    void write_bit(uint8_t v);
+    //void write_bit(uint8_t v);
+    void write_bit1(uint8_t v);
+    void write_bit2(uint8_t v);
 
     // Read a bit.
-    uint8_t read_bit(void);
+    uint8_t read_bit1(void);
+    uint8_t read_bit2(void);
 
     // Stop forcing power onto the bus. You only need to do this if
     // you used the 'power' flag to write() or used a write_bit() call
     // and aren't about to do another read or write. You would rather
     // not leave this powered if you don't have to, just in case
     // someone shorts your bus.
-    void depower(void);
+    void depower1(void);
+    void depower2(void);
 
 #if ONEWIRE_SEARCH
     // Clear the search state so that if will start from the beginning again.
-    void reset_search();
+    void reset_search1();
+    void reset_search2();
 
     // Setup the search to find the device type 'family_code' on the next call
     // to search(*newAddr) if it is present.
-    void target_search(uint8_t family_code);
+    void target_search1(uint8_t family_code);
+    void target_search2(uint8_t family_code);
 
     // Look for the next device. Returns 1 if a new address has been
     // returned. A zero might mean that the bus is shorted, there are
@@ -130,7 +145,8 @@ class OneWire
     // might be a good idea to check the CRC to make sure you didn't
     // get garbage.  The order is deterministic. You will always get
     // the same devices in the same order.
-    bool search(uint8_t *newAddr, bool search_mode = true);
+    bool search1(uint8_t *newAddr, bool search_mode = true);
+    bool search2(uint8_t *newAddr, bool search_mode = true);
 #endif
 
 #if ONEWIRE_CRC
